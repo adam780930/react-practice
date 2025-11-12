e8bc8844151d3a280a1bd909a744c1e567acc9d4
Adam Tsai <adam780930@gmail.com>
2025-11-11 22:47:29 -0800

    adding a component with function to change text dynamically to clipboard text

---
+++ src/components/Clipboard.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Clipboard() {
+  const [clipText, setClipText] = useState("");
+
+  async function getClipboardText() {
+    try {
+      const text = await navigator.clipboard.readText();
+      console.log("Clipboard text:", text);
+      setClipText(text);
+      return text;
+    } catch (err) {
+      console.error("Failed to read clipboard contents: ", err);
+      return null;
+    }
+  }
+
+  return (
+    <div>
+      <h1>
+        <button onClick={getClipboardText}>clickpls</button>
+      </h1>
+      <p>{clipText}</p>
+    </div>
+  );
+}
