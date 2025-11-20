5349c1b65c999568ea10e9881b0ca68862d5a2c3
Adam Tsai <adam780930@gmail.com>
2025-11-19 23:16:22 -0800

    cleared up and move current contents to mainpage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,16 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Mainpage from "./pages/Mainpage.jsx";
 import Submainpage from "./pages/Submainpage.jsx";
-import Navigation from "./components/Navigation.jsx";
-import Clipboards from "./components/Clipboards.jsx";
-import Button from "./components/Button.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <Navigation />
-        <div className="card">
-          <Button
-            onClick={() => setCount((count) => count + 1)}
-            buttonName={`count is ${count}`}
-          />
-        </div>
-        <span className="read-the-docs">
-          <h1>example text 1</h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet atque
-          odit dolores! Perspiciatis qui esse vero corrupti deserunt velit
-          repellendus iure nemo eos, quaerat, officiis fuga explicabo accusamus.
-          Eum, cum.
-        </span>
-        <span className="read-the-docs">
-          <h1>example text 2</h1>
-          <span>First Name: John</span>
-          <hr></hr>
-          <span>Last Name: Doe</span>
-          <hr></hr>
-          <span>Gender: Male</span>
-          <hr></hr>
-          <span>Age: 28</span>
-        </span>
-        <Clipboards />
-      </div>
+    <div>
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/sub" element={<Submainpage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,3 +1,39 @@
-export default function Mainpage() {
-  return <div>This is main page</div>;
+import { useState } from "react";
+import Navigation from "../components/Navigation";
+import Clipboards from "../components/Clipboards";
+import Button from "../components/Button";
+
+function Mainpage() {
+    const [count, setCount] = useState(0);
+  return (
+    <div>
+            <Navigation />
+            <div className="card">
+              <Button
+                onClick={() => setCount((count) => count + 1)}
+                buttonName={`count is ${count}`}
+              />
+            </div>
+            <span className="read-the-docs">
+              <h1>example text 1</h1>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet atque
+              odit dolores! Perspiciatis qui esse vero corrupti deserunt velit
+              repellendus iure nemo eos, quaerat, officiis fuga explicabo accusamus.
+              Eum, cum.
+            </span>
+            <span className="read-the-docs">
+              <h1>example text 2</h1>
+              <span>First Name: John</span>
+              <hr></hr>
+              <span>Last Name: Doe</span>
+              <hr></hr>
+              <span>Gender: Male</span>
+              <hr></hr>
+              <span>Age: 28</span>
+            </span>
+            <Clipboards />
+          </div>
+  );
 }
+
+export default Mainpage;
