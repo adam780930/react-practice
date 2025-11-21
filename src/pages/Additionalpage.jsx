e5a57c0e1e4a7c278541fd93b084eda850f15a65
Adam Tsai <adam780930@gmail.com>
2025-11-20 22:24:17 -0800

    added react-markdown library with basic setup

--- src/pages/Additionalpage.jsx
+++ src/pages/Additionalpage.jsx
@@ -1,3 +1,16 @@
-export default function Additionalpage() {
-  return <div>this is additional page</div>;
+import Markdown from "react-markdown";
+
+function Additionalpage() {
+  const markdownContent = `# Hello, Markdown!
+      This is some **bold** text and *italic* text.
+      - Item 1
+      - Item 2`;
+
+  return (
+    <div>
+      <Markdown>{markdownContent}</Markdown>
+    </div>
+  );
 }
+
+export default Additionalpage;
