4580f3eaaef7670ebd2f81931e6a18597d1b3a79
Adam Tsai <adam780930@gmail.com>
2025-11-23 21:16:42 -0800

    updated vite config to import md files correctly to allow contents from md files be directly rendered

--- src/pages/Markdownpage.jsx
+++ src/pages/Markdownpage.jsx
@@ -1,23 +1,10 @@
 import Markdown from "react-markdown";
-import { useState } from "react";
-// import firstmd from "../components/markdowndocs/firstmarkdown.md";
+import sampleMd from "../components/markdowndocs/Exampletext.md";
 
 function Markdownpage() {
-  const [firstmdContent, setFirstmdContent] = useState("");
-  const markdownContent = `# Hello, Markdown!
-      This is some **bold** text and *italic* text.
-      - Item 1
-      - Item 2`;
-
-//   fetch(firstmd)
-//     .then((response) => response.text())
-//     .then((text) => setFirstmdContent(text))
-//     .catch((error) => console.error("Error fetching markdown:", error));
-
   return (
     <div>
-      <Markdown>{markdownContent}</Markdown>
-      {/* <Markdown>{firstmdContent}</Markdown> */}
+      <Markdown>{sampleMd}</Markdown>
     </div>
   );
 }
