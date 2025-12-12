722fddabbf71040437d2ad05a275ba02f8ee8f6b
Adam Tsai <adam780930@gmail.com>
2025-12-11 18:35:56 -0800

    setting up tailwindcss

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -5,7 +5,9 @@ function Introduction() {
   return (
     <div>
       <div>this is Introductions page</div>
-      <Markdown>{introduction}</Markdown>
+      <div className="bg-stone-800">
+      <Markdown >{introduction}</Markdown>
+      </div>
     </div>
   );
 }
