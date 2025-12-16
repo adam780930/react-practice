dd156b24c27a0d21974767b2c36989ec49c496d8
Adam <100063577+adam780930@users.noreply.github.com>
2025-12-15 21:46:45 -0800

    Update/page width matching navbar (#7)
    * updated the styling for introductions page and md file
    
    * updated all current pages width to match navbar

--- src/pages/Examplepage.jsx
+++ src/pages/Examplepage.jsx
@@ -1,3 +1,7 @@
 export default function Examplepage() {
-  return <div>this is example page</div>;
+  return (
+    <div clasName="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
+      <div>this is example page</div>
+    </div>
+  );
 }
--- src/pages/Submainpage.jsx
+++ src/pages/Submainpage.jsx
@@ -1,3 +1,7 @@
 export default function Submainpage() {
-  return <div>this is submain page</div>;
+  return (
+    <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
+      <div>this is submain page</div>
+    </div>
+  );
 }
