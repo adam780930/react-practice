7663b4925687117d8160fbf68cf8fbfef5d177b9
Adam Tsai <adam780930@gmail.com>
2025-12-15 21:45:11 -0800

    updated all current pages width to match navbar

--- src/pages/Markdownpage.jsx
+++ src/pages/Markdownpage.jsx
@@ -3,7 +3,7 @@ import sampleMd from "../components/markdowndocs/Exampletext.md";
 
 function Markdownpage() {
   return (
-    <div>
+    <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
       <Markdown>{sampleMd}</Markdown>
     </div>
   );
