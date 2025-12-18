2359d8065acf9a740ce7a87e1b042503440d3c46
Adam Tsai <adam780930@gmail.com>
2025-12-17 20:05:41 -0800

    updated styling and added project URL

--- src/components/ExampleProjects.jsx
+++ src/components/ExampleProjects.jsx
@@ -4,20 +4,47 @@ import recipes from "../assets/recipes.png";
 import decoder from "../assets/decoder.png";
 
 function ExampleProjects() {
+  const projectLinks = {
+    reservation: "https://reservationapp.adamtsaidev.com/",
+    flashcard: "https://flashcardapp.adamtsaidev.com/",
+    decoder: "https://decoderapp.adamtsaidev.com/",
+    recipes: "https://recipeapp.adamtsaidev.com/",
+  };
+
   return (
     <div>
       <div className="grid grid-cols-2 gap-4">
-        <div className="border-2 border-gray-400 p-4">
-          <img src={reservation}></img>
+        <div className="border-2 border-gray-400 p-4 cursor-pointer hover:bg-stone-600">
+          <img
+            src={reservation}
+            onClick={() => {
+              window.open(projectLinks.reservation);
+            }}
+          ></img>
         </div>
-        <div className="border-2 border-gray-400 p-4">
-          <img src={flashcard}></img>
+        <div className="border-2 border-gray-400 p-4 cursor-pointer hover:bg-stone-600">
+          <img
+            src={flashcard}
+            onClick={() => {
+              window.open(projectLinks.flashcard);
+            }}
+          ></img>
         </div>
-        <div className="border-2 border-gray-400 p-4">
-          <img src={recipes}></img>
+        <div className="border-2 border-gray-400 p-4 cursor-pointer hover:bg-stone-600">
+          <img
+            src={decoder}
+            onClick={() => {
+              window.open(projectLinks.decoder);
+            }}
+          ></img>
         </div>
-        <div className="border-2 border-gray-400 p-4">
-          <img src={decoder}></img>
+        <div className="border-2 border-gray-400 p-4 cursor-pointer hover:bg-stone-600">
+          <img
+            src={recipes}
+            onClick={() => {
+              window.open(projectLinks.recipes);
+            }}
+          ></img>
         </div>
       </div>
     </div>
