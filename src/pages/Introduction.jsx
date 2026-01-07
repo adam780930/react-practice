d26c7d7badbdc0bb9e96ee2d5bba31b0cc9dfdbb
Adam Tsai <adam780930@gmail.com>
2026-01-06 17:18:15 -0800

    updated styling

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -5,11 +5,16 @@ import introduction from "../components/markdowndocs/introductions.md";
 function Introduction() {
   return (
     <div>
-      
       <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
-        <img src={Portrait} className="w-50 h-50 rounded-full border"></img>
-        <span>Hello, I am Adam.</span>
-        <Markdown>{introduction}</Markdown>
+        <div class="grid grid-cols-2">
+          <div className="m-auto p-auto">
+            <img src={Portrait} className="w-70 h-70 rounded-full border"></img>
+          </div>
+          <div>
+            <span>Hello, I am Adam.</span>
+            <Markdown>{introduction}</Markdown>
+          </div>
+        </div>
       </div>
     </div>
   );
