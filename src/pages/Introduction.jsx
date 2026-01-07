96c9f08540d3c62ca7724abba33cec10ebbfd771
Adam Tsai <adam780930@gmail.com>
2026-01-06 17:12:26 -0800

    added portait for introduciton

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,10 +1,13 @@
 import Markdown from "react-markdown";
+import Portrait from "../assets/Portrait.png";
 import introduction from "../components/markdowndocs/introductions.md";
 
 function Introduction() {
   return (
     <div>
+      
       <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
+        <img src={Portrait} className="w-50 h-50 rounded-full border"></img>
         <span>Hello, I am Adam.</span>
         <Markdown>{introduction}</Markdown>
       </div>
