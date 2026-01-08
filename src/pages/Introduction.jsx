9be40bada95362028996ff10529fcf62a654c1ee
Adam Tsai <adam780930@gmail.com>
2026-01-07 21:01:32 -0800

    updated markdown document contents and remove root styling to allow full tailwind customization for each individual components

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -6,12 +6,11 @@ function Introduction() {
   return (
     <div>
       <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
-        <div class="grid grid-cols-2">
+        <div class="grid grid-cols-2 p-4">
           <div className="m-auto p-auto">
             <img src={Portrait} className="w-70 h-70 rounded-full border"></img>
           </div>
-          <div>
-            <span className="text-3xl">Hello, I am Adam.</span>
+          <div className="markdown-content">
             <Markdown>{introduction}</Markdown>
           </div>
         </div>
