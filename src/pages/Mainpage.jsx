71bb89ae3e5b6ebb42914cff86580dd4f9ffe8e5
Adam Tsai <adam780930@gmail.com>
2026-01-10 23:39:31 -0800

    created the experience component

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,7 +1,9 @@
+import Experiences from "../components/Experiences.jsx";
+
 function Mainpage() {
   return (
     <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
-      <div>pending content</div>
+      <Experiences />
     </div>
   );
 }
