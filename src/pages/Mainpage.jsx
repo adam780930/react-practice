c07e737899e44d1c75a4500f81d6c9af5f734cf2
Adam Tsai <adam780930@gmail.com>
2026-01-10 23:49:22 -0800

    added buton to experiences page

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,8 +1,10 @@
 import Experiences from "../components/Experiences.jsx";
+import Buttons from "../components/Button.jsx";
 
 function Mainpage() {
   return (
     <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
+      <Buttons buttonName={'Experiences'}/>
       <Experiences />
     </div>
   );
