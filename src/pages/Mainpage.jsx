03741463c30d2db48251095031613932adee15b4
Adam Tsai <adam780930@gmail.com>
2026-01-11 21:24:59 -0800

    created modal component

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Experiences from "../components/Experiences.jsx";
 import Buttons from "../components/Button.jsx";
+import Modal from "../components/Modal.jsx";
 
 function Mainpage() {
-  const [modal, SetModal] = useState(
-    "fixed inset-0 bg-black bg-opacity-50 hidden flex items-center justify-center z-50"
-  );
-  const modalClass =
-    "fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50";
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false)
 
   return (
     <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
@@ -17,33 +16,13 @@ function Mainpage() {
 
       <Buttons
         buttonName={"Experiences"}
-        onClick={() => {
-          SetModal(modalClass);
-        }}
+        onClick={handleOpenModal}
       />
-
-      <div
-        id="modal-overlay"
-        className={modal}
-      >
-        <div className="bg-white p-6 rounded shadow-lg w-1/2">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-xl font-semibold">Modal Title</h2>
-            <button
-              id="close-modal"
-              className="text-gray-500 hover:text-gray-700"
-            >
-              &times;
-            </button>
-          </div>
-          <Experiences />
-          <div className="mt-4 flex justify-end">
-            <button id="close-modal-footer" className="p-2 bg-gray-300 rounded">
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
+      
+      <Modal show={isModalOpen} handleClose={handleCloseModal}>
+        <Experiences />
+      </Modal>
+  
     </div>
   );
 }
