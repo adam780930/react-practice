7391acd973a2eb96a247a57a5bd559b4905d3cc6
Adam Tsai <adam780930@gmail.com>
2026-01-12 22:11:11 -0800

    corrected tailwindcss lint error

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ function Navigation() {
   return (
     <div>
       <header>
-        <nav className="block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300 bg-opacity-90 sticky top-3 shadow lg:px-8 lg:py-3 backdrop-blur-lg backdrop-saturate-150 z-[9999]">
+        <nav className="block w-full max-w-5xl px-4 py-2 mx-auto bg-stone-300 bg-opacity-90 sticky top-3 shadow lg:px-8 lg:py-3 backdrop-blur-lg backdrop-saturate-150 z-9999">
           <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
             <Link to="/">
               <div className="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
--- src/pages/Additionalpage.jsx
+++ src/pages/Additionalpage.jsx
@@ -1,6 +1,6 @@
 function Additionalpage() {
   return (
-    <div className="bg-stone-block w-full max-w-screen-lg px-4 py-2 mx-auto bg-stone-300">
+    <div className="bg-stone-block w-full max-w-5xl px-4 py-2 mx-auto bg-stone-300">
       <span>this is additional page</span>
     </div>
   );
