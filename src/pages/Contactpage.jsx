bb8509e569847cb6f67ba80223014c30315621fc
Adam Tsai <adam780930@gmail.com>
2026-01-15 10:53:51 -0800

    implemented emailJS and updated button function to work dynamically between different components

--- src/pages/Contactpage.jsx
+++ src/pages/Contactpage.jsx
@@ -1,15 +1,29 @@
+import emailjs from "emailjs-com";
 import Buttons from "../components/Button.jsx";
 
 function ContactPage() {
+  function Emailsubmit(e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_xtgzfki",
+      "template_9ll93ma",
+      e.target,
+      "user_kh1diuAotFnI96MCgonLa"
+    );
+    e.target.reset();
+    alert("Thank you for your message!");
+  }
+
   return (
     <div className="bg-stone-block w-full max-w-5xl px-4 py-2 mx-auto bg-stone-300">
-      <form className="max-w-full mx-auto">
+      <form className="max-w-full mx-auto" onSubmit={Emailsubmit}>
         <header className="mb-5 my-2 text-center text-3xl">
           <span>Send me an email</span>
         </header>
-        <div className="mb-5">
+        <div className="form-group mb-5">
           <label
-            for="name"
+            htmlFor="name"
             className="block mb-2.5 text-sm font-medium text-heading"
           >
             Your Name
@@ -19,12 +33,12 @@ function ContactPage() {
             required
             id="name"
             name="name"
-            className="bg-gray-300 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+            className="form-control bg-gray-300 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
           />
         </div>
-        <div className="mb-5">
+        <div className="form-group mb-5">
           <label
-            for="email"
+            htmlFor="email"
             className="block mb-2.5 text-sm font-medium text-heading"
           >
             Your Email
@@ -34,12 +48,12 @@ function ContactPage() {
             required
             id="email"
             name="email"
-            className="bg-gray-300 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+            className="form-control bg-gray-300 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
           />
         </div>
-        <div className="mb-5">
+        <div className="form-group mb-5">
           <label
-            for="subject"
+            htmlFor="subject"
             className="block mb-2.5 text-sm font-medium text-heading"
           >
             Subject
@@ -49,12 +63,12 @@ function ContactPage() {
             required
             id="subject"
             name="subject"
-            className="bg-gray-300 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+            className="form-control bg-gray-300 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
           />
         </div>
-        <div className="mb-5">
+        <div className="form-group mb-5">
           <label
-            for="message"
+            htmlFor="message"
             className="block mb-2.5 text-sm font-medium text-heading"
           >
             Message
@@ -69,7 +83,7 @@ function ContactPage() {
           ></textarea>
         </div>
         <div className="mb-5 p-2 hover:opacity-50 text-center">
-          <Buttons buttonName={"Send Message"} />
+          <Buttons buttonName={"Send Message"} buttonType={"submit"}/>
         </div>
       </form>
     </div>
