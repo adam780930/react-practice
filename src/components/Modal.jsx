659a28a9e02a8b7aea774291ab1e42f9bf5522f7
Adam Tsai <adam780930@gmail.com>
2026-01-16 12:58:41 -0800

    updated modal to a reusable component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,19 @@
-
-
-const Modal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
-
+const Modal = ({ modalContent, closeButton }) => {
   return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        {children}
-        <button type="button" onClick={handleClose}>
-          Close
-        </button>
-      </section>
+    <div>
+        <div className="fixed inset-0  bg-stone-900/75 flex items-center justify-center z-50">
+          <div className="rounded-lg shadow-xl w-full max-w-3xl mx-4 bg-stone-300">
+            <div className="p-4 border-b">
+              <h1 className="text-3xl font-semibold">Experiences</h1>
+            </div>
+            <div className="p-4 max-h-[70vh] overflow-y-auto">
+              {modalContent ? modalContent : ""}
+            </div>
+            <div className="p-4 border-t flex justify-end gap-4 hover:opacity-50">
+              {closeButton ? closeButton : ""}
+            </div>
+          </div>
+        </div>
     </div>
   );
 };
