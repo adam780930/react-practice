b55f72658ac067f6165fea38eda93d4b9313062f
Adam Tsai <adam780930@gmail.com>
2026-02-06 15:12:33 -0800

    implmented themetoggle but pending bug fix on toggle

--- src/components/Themetoggle.jsx
+++ src/components/Themetoggle.jsx
@@ -1,26 +1,54 @@
-import Buttons from "./Button.jsx";
+function ThemeToggle() {
+  // Example toggle script (include in <head> to prevent FOUC)
+  const toggle = document.getElementById("theme-toggle");
+  const setDark = (isDark) => {
+    document.documentElement.classList.toggle("dark", isDark);
+    localStorage.setItem("color-theme", isDark ? "dark" : "light");
+  };
 
-function ThemeSwitch() {
-    let darkMode = false;
+  // Initial state
+  setDark(
+    localStorage.getItem("color-theme") === "dark" ||
+      (!localStorage.getItem("color-theme") &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches),
+  );
+
+  // Event listener
+  // toggle.addEventListener("click", () =>
+  //   setDark(!document.documentElement.classList.contains("dark")),
+  // );
+
   return (
     <div>
-        <Buttons buttonName={"Dark"} />
       <button
-        type="button"
-        className="hs-dark-mode hs-dark-mode-active:hidden block border rounded p-2"
-        data-hs-theme-click-value="dark"
+        id="theme-toggle"
+        className="p-2 rounded-full bg-gray-200 dark:bg-gray-700"
       >
-        Dark
-      </button>
-      <button
-        type="button"
-        className="hs-dark-mode hs-dark-mode-active:inline-flex hidden border rounded p-2"
-        data-hs-theme-click-value="light"
-      >
-        Light
+        {/* <!-- Sun Icon (for when in dark mode) - initially hidden in light mode --> */}
+        <svg
+          id="theme-toggle-light-icon"
+          className="w-6 h-6 text-gray-800 hidden dark:block"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org"
+        >
+          light
+        </svg>
+        {/* <!-- Moon Icon (for when in light mode) - initially hidden in dark mode --> */}
+        <svg
+          id="theme-toggle-dark-icon"
+          className="w-6 h-6 text-gray-800 dark:hidden"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org"
+        >
+          dark
+        </svg>
       </button>
     </div>
   );
 }
 
-export default ThemeSwitch;
+export default ThemeToggle;
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -4,6 +4,7 @@ import ExampleProjects from "../components/ExampleProjects.jsx";
 import Buttons from "../components/Button.jsx";
 import Experiences from "../components/Experiences.jsx";
 import Modal from "../components/Modal.jsx";
+import Submain from "./Submainpage.jsx";
 
 function Mainpage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,9 +12,14 @@ function Mainpage() {
   return (
     <>
       <div className="pb-8">
+        <div className="bg-stone-block w-full max-w-5xl px-4 py-2 mx-auto bg-stone-300 text-black rounded dark:bg-black dark:text-white">
+          <Submain />
+        </div>
+      </div>
+      <div className="pb-8">
         <Introductions handleOpenModal={handleOpenModal} />
       </div>
-      <div className="bg-stone-block w-full max-w-5xl px-4 py-2 mx-auto bg-black text-white rounded">
+      <div className="bg-stone-block w-full max-w-5xl px-4 py-2 mx-auto bg-stone-300 rounded">
         <div>
           <ExampleProjects />
         </div>
