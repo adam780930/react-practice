b13c4bfb4e51d69a0417f577f3e19a221e7ac512
Adam Tsai <adam780930@gmail.com>
2026-02-24 13:48:58 -0800

    current final version of the styling

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -7,8 +7,13 @@ import Experiences from "../components/Experiences.jsx";
 import Modal from "../components/Modal.jsx";
 
 function Introduction() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handleOpenModal = () => setIsModalOpen(!isModalOpen);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = () => setIsModalOpen(!isModalOpen);
+
+  const customReactComponents = {
+    h1: ({ node, ...props }) => <h1 style={{ color: "gray" }} {...props} />,
+    a: ({ node, ...props }) => <a style={{ color: "blue" }} {...props} />,
+  };
   return (
     <div>
       <div className="bg-stone-block w-full max-w-5xl px-4 py-2 mx-auto bg-stone-300 text-black rounded dark:bg-black dark:text-white">
@@ -23,19 +28,21 @@ function Introduction() {
             </div>
           </div>
           <div className="prose dark:text-white">
-            <Markdown>{introduction}</Markdown>
+            <Markdown components={customReactComponents}>
+              {introduction}
+            </Markdown>
           </div>
         </div>
       </div>
       <div className="flex justify-center"></div>
-        {isModalOpen && (
-          <Modal
-            modalContent={<Experiences />}
-            closeButton={
-              <Buttons buttonName={"Close"} onClick={handleOpenModal} />
-            }
-          />
-        )}
+      {isModalOpen && (
+        <Modal
+          modalContent={<Experiences />}
+          closeButton={
+            <Buttons buttonName={"Close"} onClick={handleOpenModal} />
+          }
+        />
+      )}
     </div>
   );
 }
